bc00943c0c613591323b6f0e87afcdbd15c8b617
ADIMALD <malikadisa006@gmail.com>
2026-01-13 18:03:31 +0100

    Final fix for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/chatbot-project/",
+  base: "/", // Vercel needs the root path
   plugins: [react()],
 })
